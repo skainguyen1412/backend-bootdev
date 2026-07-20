d5260ca241663197bcb19c0bff12bf35ad02a3a4
spaceman1412 <spacemannormal@gmail.com>
2026-07-20 14:24:49 +0700

    Finish c2l1

--- web-scraper/main.py
+++ web-scraper/main.py
@@ -1,5 +1,21 @@
+import sys
+
+
 def main():
     print("Hello from web-scraper!")
+
+    argument_len = len(sys.argv)
+
+    if argument_len < 2:
+        print("no website provided")
+        sys.exit(1)
+
+    if argument_len > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    BASE_URL = sys.argv[1]
+    print(f"starting crawl of: {BASE_URL}")
 
 
 if __name__ == "__main__":
